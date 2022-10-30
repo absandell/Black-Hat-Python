f6b954b755f03bb9feee592533a18ead828b6c9d
absandell <asandell@ufl.edu>
2022-10-30 16:21:55 -0400

    Adds Some Networking exercises

---
+++ Networking/TCP-Proxy.py
@@ -0,0 +1,50 @@
+from sqlite3 import connect
+import sys
+import socket
+import threading
+
+HEX_FILTER = ''.join(
+    [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)]
+)
+
+def hexdump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
+
+    results = list()
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+
+        printable = word.translate(HEX_FILTER)
+        hexa = ' '.join([f'{ord(c):02x}' for c in word])
+        hexwidth = length*3
+        results.append(f'{i:04x}  {hexa:<{hexwidth}}  {printable}')
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
+
+def request_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def response_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+if __name__ == '__main__':
+    hexdump('python rocks\n and proxies roll\n')
